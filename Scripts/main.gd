9246dfd728a07d4259b0263b7470a5a9f92c57de
Arknoid <omalige@gmail.com>
2017-12-05 22:37:37 +0100

    progressif zoom

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,6 +1,8 @@
 extends Node2D
 var startScreenLoaded = false
 var worldScreenLoaded = false
+func _ready():
+	get_node("Camera2D").set_scale(Vector2 (1.5,1.5))
 
 func _on_Timer_timeout():
 	get_node("loader").queue_free()
@@ -12,7 +14,8 @@ func goStartScreen():
 		var start = preload("res://Scenes/Start.tscn").instance()
 		add_child(start)
 		startScreenLoaded = true
-		get_node("Camera2D").set_zoom(Vector2(1.2,1.2))
+		get_node("animCamera").play("Start")
+
 
 func goWorldScreen():
 	
@@ -20,10 +23,11 @@ func goWorldScreen():
 		var world = preload("res://Scenes/World.tscn").instance()
 		add_child(world)
 		worldScreenLoaded = true
-		get_node("Camera2D").set_zoom(Vector2(1,1))
+		get_node("animCamera").play("startToWorld")
 
 func goGameOverScreen():
 	worldScreenLoaded = false
+	get_node("animCamera").play("worldToGameOver")
 	var gameOver = preload("res://Scenes/gameOver.tscn").instance()
 	add_child(gameOver)
 	
